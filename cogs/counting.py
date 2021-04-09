7ba41cec49120452fb9b3b6084c38b05f0c02381
Noah Thorn <41551584+thatnerdnoah@users.noreply.github.com>
2021-04-09 19:35:42 -0400

    Added counting cog

---
+++ cogs/counting.py
@@ -0,0 +1,47 @@
+from discord import TextChannel, Embed
+from discord.ext import commands, tasks
+from discord.ext.commands import bot
+
+import config
+
+class Counting(commands.Cog, name="Counting"):
+    def __init__(self) -> None:
+        self.bot: commands.Bot = bot
+        self.counting_channel : TextChannel = None
+        self.counter = 0
+        self.expected_number = 1
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.counting_channel = self.bot.get_channel(config.counting_channel)
+        print("Counting begins!")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.channel == self.counting_channel:
+            if message.author == self.bot.user:
+                return
+            else:
+                try:
+                    message_number = int(message.content)
+                    if message_number == self.expected_number:
+                        self.counter += 1
+                        self.expected_number += 1
+                        await message.add_reaction("✅")
+                    else:
+                        self.counter = 0
+                        self.expected_number = 1
+                        await message.add_reaction("❌")
+                        await self.bot.send(f"<@{message.author.id}> cant count! :megasRage:")
+                except Exception as error:
+                    await message.add_reaction("❌")
+                    await self.bot.send("Something went wrong...")
+                    print(error)   
+        else: 
+            return
+            
+
+
+
+def setup(client):
+    client.add_cog(Counting(client))
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from os import environ
 token = environ["DISCORD_TOKEN"]
 mod_channel = int(environ["MOD_CHANNEL"])
 log_channel = int(environ["LOG_CHANNEL"])
+counting_channel = int(environ["COUNTING_CHANNEL"])
 cogs = ["cogs.item_shop", "cogs.logger"]
 prefix = "$"
 shop_channel = int(environ["FORTNITE_CHANNEL"])

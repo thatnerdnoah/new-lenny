62f96f61c8156241504ce43298faf7b3b1daf376
Noah Thorn <41551584+thatnerdnoah@users.noreply.github.com>
2021-12-31 15:43:49 -0500

    warning cog

--- cogs/warning.py
+++ cogs/warning.py
@@ -23,7 +23,7 @@ class Warning(commands.Cog, name="Moderation"):
 
     @commands.command(name="warn", aliases=['w'])
     async def warning(self, ctx, user_id: int):
-        """Warns the user of their actions and grants the user strikes. The reason must be in quotation marks."""
+        """Warns the user of their actions. The reason must be in quotation marks."""
         channel = ctx.channel
 
         await ctx.channel.send("Please state the reason for the warning in your next message.")
@@ -32,13 +32,16 @@ class Warning(commands.Cog, name="Moderation"):
             return m.channel == channel
 
         reason: str = await self.bot.wait_for("message", check=check)
+
+        print(reason)
         
-        user = self.bot.get_user(user_id)
+        # user = self.bot.get_user(user_id)
+        user = ctx.message.server.get_member(user_id)
         await user.send(
             f"The moderators of {config.discord_name} have given you a warning for the following reason: \n\n{reason}\n\nPlease review the rules and be careful not to obtain more strikes. If you have any questions, message one of the moderators."
         )
         await print("Message sent")
-        await ctx.message.add_reaction('✅')
+        await reason.add_reaction('✅')
         
 
 def setup(client):

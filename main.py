c95145bd54c09ebdd8615c5c2c40253267bf2fd0
Noah Thorn <41551584+thatnerdnoah@users.noreply.github.com>
2022-09-02 19:43:21 -0400

    update API

--- main.py
+++ main.py
@@ -20,17 +20,15 @@ async def on_ready():
     print(f"Logged in as {bot.user.name}!")
 
 
-async def main():
+async def add_extensions():
     for cog in config.cogs:
         try:
-            await bot.add_cog(cog)
+            bot.load_extension(cog)
             print(f"Loaded extension {cog}")
         except Exception as error:
             print(f"Cog {cog} could not be loaded. Reason: {error}")
             traceback.print_exc()
 
-    bot.run(config.token)
 
-
-if __name__ == "__main__":
-    main()
+add_extensions()
+bot.run(config.token)

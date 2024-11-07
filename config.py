031707e631eb119c6116ed3f364d81eac9383f52
Noah Thorn <41551584+thatnerdnoah@users.noreply.github.com>
2024-11-06 19:05:37 -0500

    Eliminated some config variables

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import environ
 
 token = environ["DISCORD_TOKEN"]
-admin_role = environ["MOD_ROLE"]
+# admin_role = environ["MOD_ROLE"]
 mod_channel = int(environ["MOD_CHANNEL"])
 log_channel = int(environ["LOG_CHANNEL"])
 counting_channel = int(environ["COUNTING_CHANNEL"])
@@ -9,8 +9,8 @@ cogs = ["cogs.counting"]
 bot_id = 597109548104548390
 prefix = "$"
 project_id = environ["PROJECT_ID"]
-discord_name = "The Snoop Troop"
+discord_name = "Snoop Troop"
 local_test = False
 
-url = 'https://api.fortnitetracker.com/v1/store'
-headers = {'TRN-Api-Key': f'{environ["FORTNITE_KEY"]}'}
+# url = 'https://api.fortnitetracker.com/v1/store'
+# headers = {'TRN-Api-Key': f'{environ["FORTNITE_KEY"]}'}

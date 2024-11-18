166a594192de6bca2ef960c138628a34b023cf95
Noah Thorn <41551584+thatnerdnoah@users.noreply.github.com>
2024-11-18 09:54:30 -0500

    Large code refactor

---
+++ helpers/database.py
@@ -0,0 +1,90 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+local_test = False
+
+try:
+    import config_local as config
+    local_test = True
+except ImportError:
+    import config
+
+cred = credentials.Certificate("./helpers/lennydb-94aae-firebase-adminsdk-lp085-9c8442a238.json")
+firebase_admin.initialize_app(cred)
+ 
+db = firestore.client()
+
+def database_pull():
+    expected_number : int = 0
+    record : int = 0
+    lives : int = 0
+
+    if not config.local_test:
+        doc_ref = db.collection(u'counting').document(u'count')
+    else:
+        doc_ref = db.collection(u'counting').document(u'count_test')
+        
+
+    doc = doc_ref.get()
+    if doc.exists:
+        expected_number = doc.to_dict()['count']
+        record = doc.to_dict()['reward']
+        lives = doc.to_dict()['lives']
+
+    return expected_number, record, lives
+
+def update_record(num: int):
+    if not config.local_test:
+        counter_ref = db.collection(u'counting').document(u'count')
+    else:
+        counter_ref = db.collection(u'counting').document(u'count_test')
+
+    counter_ref.update({
+        u'reward': num
+    })
+
+def database_push(num: int):
+    if not config.local_test:
+        counter_ref = db.collection(u'counting').document(u'count')
+    else:
+        counter_ref = db.collection(u'counting').document(u'count_test')
+
+    counter_ref.update({
+        u'count': num
+    })
+
+def update_lives(num: int):
+    if not config.local_test:
+        counter_ref = db.collection(u'counting').document(u'count')
+    else:
+        counter_ref = db.collection(u'counting').document(u'count_test')
+
+    counter_ref.update({
+        u'lives': num
+    })
+
+def database_copy(num: int):
+    if not config.local_test:
+        counter_ref = db.collection(u'counting').document(u'count')
+    else:
+        counter_ref = db.collection(u'counting').document(u'count_test')
+    
+    if num >= 1:
+        counter_ref.update ({
+            u'count_backup': num
+        })
+
+def pull_backup():
+    backup_numnber: int = 0
+    
+    if not config.local_test:
+        doc_ref = db.collection(u'counting').document(u'count')
+    else:
+        doc_ref = db.collection(u'counting').document(u'count_test')
+
+    doc = doc_ref.get()
+    if doc.exists:
+        backup_numnber = doc.to_dict()['count_backup']
+
+    return backup_numnber
